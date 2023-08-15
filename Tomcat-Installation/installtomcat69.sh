a392a66f63899df03e284a02566efc4738742a80
Femi Aduroja <137192203+femiaduroja@users.noreply.github.com>
2023-08-15 17:59:08 +0100

    Update installtomcat69.sh

--- Tomcat-Installation/installtomcat69.sh
+++ Tomcat-Installation/installtomcat69.sh
@@ -1,11 +1,10 @@
 # Steps for Installing tomcat9 on rhel7&8
-# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
 sudo hostnamectl set-hostname tomcat
 cd /opt
 sudo yum install git wget vim -y
 sudo yum install java-1.8.0-openjdk-devel -y
-# Download tomcat software and extract it.
 # dowanload and extract tomcat software
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
 sudo tar -xvf apache-tomcat-9.0.69.tar.gz
